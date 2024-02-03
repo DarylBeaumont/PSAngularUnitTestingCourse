0df542eb278296bcbf01d9d7d89d15058475f152
DarylBeaumont <darylbeaumont@gmail.com>
2024-02-03 22:23:10 +0000

    Added a deep integration test
    Added a deep integration test for our `HeroesComponent` and `HeroService`.

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -5,6 +5,7 @@ import { HeroService } from "../hero.service";
 import { Component, Input, NO_ERRORS_SCHEMA } from "@angular/core";
 import { Hero } from "../hero";
 import { By } from "@angular/platform-browser";
+import { HeroComponent } from "../hero/hero.component";
 
 describe('HeroesComponent', () => {
     let fixture: ComponentFixture<HeroesComponent>;
@@ -49,5 +50,45 @@ describe('HeroesComponent', () => {
         fixture.detectChanges();
 
         expect(fixture.debugElement.queryAll(By.css('li')).length).toBe(3);
+    });
+});
+
+describe('HeroesComponent children', () => { 
+    let fixture: ComponentFixture<HeroesComponent>;
+    let mockHeroService;
+    let HEROES;
+
+    beforeEach(() => {
+        HEROES = [
+            {id: 1, name: 'SpiderDude', strength: 8},
+            {id: 2, name: 'Wonderful Woman', strength: 24},
+            {id: 3, name: 'SuperDude', strength: 55},
+        ];
+
+        mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+
+        TestBed.configureTestingModule({
+            declarations: [HeroesComponent, HeroComponent],
+            providers: [
+                { provide: HeroService, useValue: mockHeroService }
+            ],
+            schemas: [NO_ERRORS_SCHEMA]
+        });
+        fixture = TestBed.createComponent(HeroesComponent);
+    });
+    
+    it('should render each hero as a HeroComponent', () => { 
+        // Arrange
+        mockHeroService.getHeroes.and.returnValue(of(HEROES));
+
+        // Act - Run ngOnInit
+        fixture.detectChanges();
+
+        // Assert
+        const heroComponentDEs = fixture.debugElement.queryAll(By.directive(HeroComponent));
+        expect(heroComponentDEs.length).toBe(3);
+        for (let i = 0; i < heroComponentDEs.length; i++) {
+            expect(heroComponentDEs[i].componentInstance.hero).toEqual(HEROES[i]);
+        }
     });
 });
